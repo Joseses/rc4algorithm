9515cb1c4cc2c9339d5e16899157722d93b33f8c
joseom07 <beto_bcw_09@hotmail.com>
2017-03-27 17:46:28 -0600

    Decrypt function ready

--- src/RC4Algorithm.scala
+++ src/RC4Algorithm.scala
@@ -23,12 +23,40 @@ class RC4Algorithm(key:String) {
       t = (sArray(i) + sArray(j)) & 0xFF
       k = sArray(t)
 
+
       ciphertext += "00" + Integer.toString(plaintext(counter).toByte ^ k, 16) takeRight 2
 
       counter += 1
     }
 
     ciphertext
+  }
+
+  def decryption(ciphertext: String): String =
+  {
+    var i = 0; var j = 0; var k = 0; var t = 0; var counter = 0
+    var plaintext = ""
+    for(counter <- 0 to ciphertext.length by 2)
+      {
+        i = (i + 1) & 0xFF
+        j = (j + sArray(i)) & 0xFF
+        val temp = sArray(i)
+        sArray(i) = sArray(j)
+        sArray(j) = temp
+        t = (sArray(i) + sArray(j)) & 0xFF
+        k = sArray(t)
+
+        if (counter !=ciphertext.length ) {
+          var unSubString = ciphertext.substring(counter, counter + 2)
+          var number = Integer.parseInt(unSubString, 16)
+
+          plaintext += "00"+ Integer.toString(number.toByte^k,16) takeRight 2
+
+        }
+
+      }
+    plaintext
+
   }
 
 
@@ -57,6 +85,7 @@ class RC4Algorithm(key:String) {
 object default {
   def main(args: Array[String]): Unit = {
     val rc4 = new RC4Algorithm("ABCD")
-    print(rc4.encryption("Hello World"))
+    //println(rc4.encryption("Hello World"))
+    print(rc4.decryption("700196db60f45629a54046"))
   }
 }
