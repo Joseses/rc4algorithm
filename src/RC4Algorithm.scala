ccbb66361b86bfa1414181c25ee731b2fe55ee95
José Manuel Jiménez <cosemine.josema@gmail.com>
2017-04-06 15:35:15 -0500

    Minor changes

--- src/RC4Algorithm.scala
+++ src/RC4Algorithm.scala
@@ -1,11 +1,13 @@
 /**
   * 148100, ,
   */
-class RC4Algorithm(key:String) {
+class RC4Algorithm(key:Array[Int], arr: Array[Int]) {
 
-  var sArray = new Array[Int](256)
+  var sArray: Array[Int] = arr
   var tArray = new Array[Int](256)
-  var kArray: Array[Char] = key.toCharArray
+  var kArray: Array[Int] = key
+
+  def this(key: String) = this(key.map(_.toInt).toArray, new Array[Int](256))
 
   initialization()
   sPermutation()
@@ -14,12 +16,12 @@ class RC4Algorithm(key:String) {
     var i = 0; var j = 0; var k = 0; var t = 0; var counter = 0
     var ciphertext = ""
     while ( counter < plaintext.length ) {
-      i = (i + 1) & 0xFF
-      j = (j + sArray(i)) & 0xFF
+      i = (i + 1) & (sArray.length - 1)
+      j = (j + sArray(i)) & (sArray.length - 1)
       val temp = sArray(i)
       sArray(i) = sArray(j)
       sArray(j) = temp
-      t = (sArray(i) + sArray(j)) & 0xFF
+      t = (sArray(i) + sArray(j)) & (sArray.length - 1)
       k = sArray(t)
 
       ciphertext += "00" + Integer.toString(plaintext(counter).toInt ^ k, 16) takeRight 2
@@ -36,12 +38,12 @@ class RC4Algorithm(key:String) {
     var charArray : Array[String] = ciphertext.split("(?<=\\G..)")
 
     for(index <- charArray.indices) {
-      i = (i + 1) & 0xFF
-      j = (j + sArray(i)) & 0xFF
+      i = (i + 1) & (sArray.length - 1)
+      j = (j + sArray(i)) & (sArray.length - 1)
       val temp = sArray(i)
       sArray(i) = sArray(j)
       sArray(j) = temp
-      t = (sArray(i) + sArray(j)) & 0xFF
+      t = (sArray(i) + sArray(j)) & (sArray.length - 1)
       k = sArray(t)
 
       var number = Integer.parseInt(charArray(index), 16)
@@ -63,8 +65,8 @@ class RC4Algorithm(key:String) {
 
   def sPermutation(): Unit = {
     var j = 0
-    for(i <- 0 until 256) {
-      j = (j + sArray(i) + tArray(i)) & 0xFF
+    for(i <- 0 until (sArray.length - 1) ) {
+      j = (j + sArray(i) + tArray(i)) & (sArray.length - 1)
       swap(sArray,i,j)
     }
   }
@@ -82,6 +84,12 @@ object default {
     val rc4D = new RC4Algorithm("ABCD")
     println(rc4.encryption("Hello World"))
     println(rc4D.decryption("700196db60f45629a54046"))
+
+    val rc42 = new RC4Algorithm(Array(2, 5), Array(0, 1, 2, 3))
+    val rc42D = new RC4Algorithm(Array(2, 5), Array(0, 1, 2, 3))
+    var encrypted = rc42.encryption("hi")
+    println(encrypted)
+    println(rc42D.decryption(encrypted))
     //Beto es jo..ven
   }
 }
